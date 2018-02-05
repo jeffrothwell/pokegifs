f8e9901bdd69fe92cc976628d0d853d1291e21b6
jeffrothwell <rothwelljeff@gmail.com>
2018-02-05 14:05:12 -0500

    add id, name and types to JSON render

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -4,7 +4,14 @@ class PokemonController < ApplicationController
     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
     body = JSON.parse(res.body)
     name = body["name"]
-    render json: { "message": "#{name}" }
+    id = body["id"]
+    types = []
+    body["types"].each { |type| types << type["type"]["name"]}
+    render json: {
+      "id": id,
+      "name": "#{name}",
+      "types": types
+      }
   end
 
 end
